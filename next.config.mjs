f6b9f8f60cd2dccdf2530594536516cf5fce00ad
ed <edwardca12@hotmail.com>
2025-08-22 16:32:55 -0500

    v1.0.3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pino-pretty"],
+  webpack: (config) => {
+    // Add path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
